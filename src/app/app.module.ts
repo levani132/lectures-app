0c11ebff2a85594199a2f743f8cc53a68196b6d3
Levan Beroshvili <lberoshvili9@gmail.com>
2020-05-03 00:00:02 +0400

    Done first lecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ShellComponent } from './shell/shell.component';
+import { AccountComponent } from './shell/account/account.component';
+import { AccountsComponent } from './shell/accounts/accounts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShellComponent,
+    AccountComponent,
+    AccountsComponent
   ],
   imports: [
     BrowserModule
