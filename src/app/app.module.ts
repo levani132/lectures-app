6ad118c278a73d371b1ddb84c1832cdfa3caa523
daviti <chxei5667@gmail.com>
2020-05-06 14:12:46 +0000

    dc-seminar-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ErrorAlertComponent } from './error-alert/error-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule
