17346ad10b85aa69aa260a2c2c615e1bed259039
Levan Beroshvili <lberoshvili9@gmail.com>
2020-05-10 19:32:36 +0400

    Added Lecture 4 starting files

---
+++ src/app/shell/shell.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bg-shell',
+  templateUrl: './shell.component.html',
+  styleUrls: ['./shell.component.scss']
+})
+export class ShellComponent {
+  onlyOdd = false;
+  numbers = [1, 2, 3, 4, 5];
+}
