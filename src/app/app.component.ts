08702e7ffbf9fa203ab1cb013bb91ad510726e2f
Levan Beroshvili <lberoshvili9@gmail.com>
2020-05-15 21:56:57 +0400

    First Custom Validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { FormGroup, FormControl, Validators, AbstractControl, FormArray } from '
 export class AppComponent implements OnInit {
   genders = ['ქალი', 'კაცი', 'სხვა'];
   signupForm: FormGroup;
+  forbiddenNames = ['მარიამი', 'დიმიტრი'];
 
   ngOnInit() {
     this.initForm();
@@ -26,6 +27,12 @@ export class AppComponent implements OnInit {
     return (this.signupForm.get('hobbies') as FormArray).controls;
   }
 
+  forbiddenNamesValidator(control: FormControl): { [key: string]: any } {
+    if (this.forbiddenNames.includes(control.value)) {
+      return { nameIsForbidden: true };
+    }
+  }
+
   get(controlName: string | (string | number)[]): AbstractControl {
     return this.signupForm.get(controlName);
   }
@@ -33,7 +40,7 @@ export class AppComponent implements OnInit {
   initForm() {
     this.signupForm = new FormGroup({
       userData: new FormGroup({
-        username: new FormControl(undefined, Validators.required),
+        username: new FormControl(undefined, [Validators.required, this.forbiddenNamesValidator.bind(this)]),
         email: new FormControl(undefined, [Validators.required, Validators.email])
       }),
       gender: new FormControl('კაცი'),
