969cb63e488f3c42b6f50abc368ccc4f4da0b30c
Levan Beroshvili <lberoshvili9@gmail.com>
2020-05-15 20:00:36 +0400

    Using form value and resetting it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,23 @@ export class AppComponent {
   defaultQuestion = 'pet';
   answer;
   genders = ['ქალი', 'კაცი'];
+  user: any = {};
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
 
   onSubmit(form: NgForm) {
-    console.log(form);
+    this.submitted = true;
+    // this.user.username = form.value.userData.username;
+    // this.user.email = form.value.userData.email;
+    // this.user.secret = form.value.secret;
+    // this.user.answer = form.value.answer;
+    // this.user.gender = form.value.gender;
+    this.user = form.value;
+    form.reset({
+      userData: { username: 'Levani' }
+    });
   }
 }
