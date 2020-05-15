f545b08da71eae175fb1ef3cefc7007610d3b975
Levan Beroshvili <lberoshvili9@gmail.com>
2020-05-15 20:49:43 +0400

    Creating form group

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'bg-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   genders = ['ქალი', 'კაცი', 'სხვა'];
   signupForm: FormGroup;
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    this.signupForm = new FormGroup({
+      username: new FormControl(undefined),
+      email: new FormControl(undefined),
+      gender: new FormControl('კაცი')
+    });
+  }
 }
