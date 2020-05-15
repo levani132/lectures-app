461d5e9d9094158d8bf3683bba426ae9699479c9
Levan Beroshvili <lberoshvili9@gmail.com>
2020-05-15 20:29:06 +0400

    First task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { SeminarService } from './seminar.service';
+import { Observable, Observer } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'bg-root',
@@ -14,5 +16,19 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     // TODO: აქ დაწერეთ თქვენი interval observable-ის კოდი.
+    const timeout = time => new Observable((observer: Observer<number>) => {
+      setTimeout(() => {
+        observer.next(time);
+        observer.complete();
+      }, time);
+    });
+
+    timeout(2000).
+      pipe(map((value: number) => {
+        return 'ფუნქციის გამოძახება მოხდა ' + value + ' მილი წამის შემდეგ';
+      }))
+      .subscribe(value => {
+        console.log(value);
+      });
   }
 }
