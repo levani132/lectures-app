114692e45489042150d49f6aab6a2a0a0141fc83
Levan Beroshvili <lberoshvili9@gmail.com>
2020-05-15 16:04:56 +0400

    Added rxjs interval

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'bg-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.subscription = interval(1000).subscribe(count => {
+      console.log(count);
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
