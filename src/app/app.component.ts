8a7e91984fdeefeba7e546f8cb3abbf6c4b35158
Levan Beroshvili <lberoshvili9@gmail.com>
2020-05-19 23:28:45 +0400

    Async pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  appStatus = new Promise(resolve => {
+    setTimeout(() => resolve('აქტიური'), 2000);
+  });
   filterText = '';
   shouldFilter = true;
   accounts = [
