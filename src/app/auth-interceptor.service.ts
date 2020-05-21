a31d2613226368d6559ee334b579d1ab5837eb0d
Levan Beroshvili <lberoshvili9@gmail.com>
2020-05-21 22:42:13 +0400

    Request intercepting

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -2,7 +2,10 @@ import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http'
 
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
+    const modifiedRequest = req.clone({
+      headers: req.headers.append('Auth', 'some-token')
+    });
     console.log('Request has been intercepted');
-    return next.handle(req);
+    return next.handle(modifiedRequest);
   }
 }
