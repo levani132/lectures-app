e297e8cd6291333ca0a0905cb911c6ba98e31f1e
HidekiL <dimitri.bedinadze@gmail.com>
2020-05-23 18:15:30 +0400

    seminar 8 done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { SeminarService } from './seminar.service';
 
@@ -7,12 +9,45 @@ import { SeminarService } from './seminar.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   showCongratulations = false;
+  tm: number = 1000;
+  
+  private timeoutSubscription: Subscription;
+  private seminarSubscription: Subscription;
 
   constructor(private seminarService: SeminarService) {}
 
   ngOnInit() {
     // TODO: აქ დაწერეთ თქვენი interval observable-ის კოდი.
+    const customObservable = Observable.create(observer => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(this.tm);
+        if (this.showCongratulations) {
+          observer.complete('complete');
+        }
+        count++;
+      }, this.tm)
+    });
+
+    this.timeoutSubscription = customObservable.pipe(map((tm: number) => {
+      return `ფუნქციის გამოძახება მოხდა ${tm} მილი წამის შემდეგ`; 
+    })).subscribe((tm: number) => {
+      console.log(tm);
+    }, error => {
+      console.log('მოხდა ერორი');
+    }, () => {
+      console.log('დასრულებულია');
+    })
+
+    this.seminarSubscription = this.seminarService.seminarFinishedSubject.subscribe(showMessage => {
+      this.showCongratulations = showMessage;
+    })
+  }
+
+  ngOnDestroy() {
+    this.timeoutSubscription.unsubscribe();
+    this.seminarSubscription.unsubscribe();
   }
 }
--- src/app/seminar.service.ts
+++ src/app/seminar.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SeminarService {
-
+  seminarFinishedSubject = new Subject<boolean>();
 }
