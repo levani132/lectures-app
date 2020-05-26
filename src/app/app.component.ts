0562339fc67fb68fdbb9a6406fa03d01ab6c48d2
Giorgi <gkiltava@bog.ge>
2020-05-26 21:18:40 +0400

    GK-seminar-9 is done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { BGValidators } from './bg-validators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'bg-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'lectures-app';
+  projectForm: FormGroup;
+  sub: Subscription;
+
+  ngOnInit() {
+    this.initForm();
+    this.sub = this.projectForm.statusChanges.subscribe(console.log); // დავალება არ იყო, ისე, სატესტოდ ვცადე
+  }
+
+  initForm() {
+    this.projectForm = new FormGroup({
+      projectName: new FormControl(undefined, [BGValidators.required, BGValidators.testName]),
+      email: new FormControl(undefined, [BGValidators.required, BGValidators.email], [BGValidators.testEmail]),
+      status: new FormControl('სტაბილური')
+    });
+  }
+
+  get(control) {
+    return this.projectForm.get(control);
+  }
+
+  errors(controlName: string | (string | number)[]) {
+    return Object.values(this.get(controlName).errors);
+  }
+
+  onSubmit() {
+    console.log(this.projectForm.value);
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
 }
---
+++ src/app/bg-validators.ts
@@ -0,0 +1,29 @@
+import { Validators, FormControl } from '@angular/forms';
+
+export class BGValidators extends Validators {
+
+    static required(control: FormControl) {
+        return super.required(control) ? { required: 'გთხოვთ შეავსოთ ველი' } : undefined;
+    }
+
+    static email(control: FormControl) {
+        return super.email(control) ? { email: 'გთხოვთ მიუთითოთ სწორი მისამართი' } : undefined;
+    }
+
+    static testName(control: FormControl) {
+        if (control.value && control.value.toLowerCase() === 'test') {
+            return { testName: 'ეს სახელი აკრძალულია' };
+        }
+    }
+
+    static testEmail(control: FormControl) {
+        return new Promise(resolve => {
+            setTimeout (() => {
+                if (control.value && control.value === 'test@test.com') {
+                    return resolve({ testEmail: 'ეს მისამართი აკრძალულია' });
+                }
+                return resolve(null);
+            }, 1000);
+        });
+    }
+}
