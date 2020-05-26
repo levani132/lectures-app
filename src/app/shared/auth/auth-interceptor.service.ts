b524f14bb2cc91c92204386dfee7d2ec90f6c709
Levan Beroshvili <lberoshvili9@gmail.com>
2020-05-26 21:58:19 +0400

    Added Authorization header from auth interceptor

--- src/app/shared/auth/auth-interceptor.service.ts
+++ src/app/shared/auth/auth-interceptor.service.ts
@@ -1,10 +1,34 @@
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEventType } from '@angular/common/http';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { take, switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
+import { AuthService } from './auth.service';
+
+@Injectable()
 export class AuthInterceptorService implements HttpInterceptor {
-  intercept(req: HttpRequest<any>, next: HttpHandler) {
-    const modifiedRequest = req.clone({
-      headers: req.headers.append('Auth', 'some-token')
-    });
-    return next.handle(modifiedRequest);
+  constructor(private authService: AuthService) {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return this.authService.user.pipe(
+      take(1),
+      switchMap((user) => {
+        if (!user) {
+          return next.handle(req);
+        }
+        const newReq = req.clone({
+          headers: req.headers.append('Authorization', 'Bearer ' + user.token),
+        });
+        return next.handle(newReq);
+      })
+    );
   }
 }
